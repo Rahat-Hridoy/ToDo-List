9454bb4753b36c87c1ee59a08c8eee92fa8b067f
MD RAHATUL ISLAM <rahathridoyd2x@gmail.com>
2025-11-26 22:01:17 +0600

    complete todo list

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
 const inputTask = document.getElementById("inputTask");
 const addBtn = document.getElementById("addBtn");
 const todoList = document.getElementById("todoList");
+let todos = [];
+
+// render list
+function renderList() {
+  todoList.innerHTML = "";
+  todos.forEach(function (item) {
+    const li = document.createElement("li");
+    li.innerHTML = item + "<span>X</span>";
+    todoList.appendChild(li);
+  });
+}
+
+// input task
+function addTask(item) {
+  if (item == "") {
+    null;
+  } else {
+    todos.push(item);
+  }
+  saveToLocalStorage();
+}
+
+// input button
+addBtn.addEventListener(`click`, function () {
+  addTask(inputTask.value);
+  inputTask.value = "";
+  renderList();
+  console.log(todos);
+});
+
+// on Load
+window.onload = function () {
+  const getLocalStorageItem = localStorage.getItem("todos");
+  if (getLocalStorageItem) {
+    todos = JSON.parse(getLocalStorageItem);
+  }
+  renderList();
+};
+
+// save to local storage
+function saveToLocalStorage() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
